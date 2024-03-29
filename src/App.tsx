ff85b3b2b9a15fde8d989eb55cafba64adc450ce
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2024-03-29 14:54:34 +0300

    Added component Header and Technologies

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header";
+import Technologies from "./components/Technologies";
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Technologies/>
+        </div>
+    );
 }
+
+
+
 
 export default App;
