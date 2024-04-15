2dff8de7b2eb71c7bb0ac60c5423ad91cce2d00b
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2024-04-15 15:51:50 +0300

    breakdown into individual components (lesson 13)

---
+++ src/components/Profile.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export const Profile = () => {
+    return (
+        <div className="content">
+            <img
+                src="https://img-19.commentcamarche.net/wzKKufHO7dLH-WPFdXJHEmOmi7E=/1500x/smart/2d8c2b30aee345008ee860087f8bcdc9/ccmcms-commentcamarche/36120212.jpg"
+                alt="contentImg"/>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+            </div>
+            <div>
+                New post
+            </div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+    );
+};
